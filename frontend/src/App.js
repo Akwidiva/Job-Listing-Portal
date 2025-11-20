284999f48091fdcca59ef5aa755735c5d89f9640
urvashi <urvashipatel87935@gmail.com>
2025-11-20 15:12:01 +0530

    registration page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Fix 1: Correct import names (FP not IP)
+import Registration from './components/auth/Registration/Registration';
+import ForgotPassword from './components/auth/ForgotPassword/FP';
+import ResetPassword from './components/auth/ResetPassword/ResetPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Fix 2: Use <Route> not <Router> */}
+          <Route path="/" element={<Registration />} />
+          <Route path="/FP" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
